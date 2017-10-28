27d3c6114d7ec1f6498fcc69c7b0dd412b4e7146
mike011 <mike_ch_1@hotmail.com>
2017-10-28 17:15:26 -0400

    unpolished version

---
+++ BullsEye/BullsEye/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Michael Charland on 2017-10-28.
+//  Copyright © 2017 charland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var target: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startNewRound()
+        updateLabels()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func showAlert() {
+        let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
+
+        score += points
+        
+        let message = "You scored \(points) points"
+        let alert  = UIAlertController(title: "Hello, World", message: message,
+                                       preferredStyle: .alert)
+
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+
+        startNewRound()
+        updateLabels()
+    }
+
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+
+    func startNewRound() {
+        round += 1
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
+
+    func updateLabels() {
+        target.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+}
+
